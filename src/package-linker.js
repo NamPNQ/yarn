857068c80625546f68540c730c37b3c6995980b9
ryan_wang5 <rwang5@atlassian.com>
2022-01-20 16:22:27 +1100

    only remove top level extraneous files

--- src/package-linker.js
+++ src/package-linker.js
@@ -345,16 +345,13 @@ export default class PackageLinker {
 
               for (const subfile of await fs.readdir(filepath)) {
                 possibleExtraneous.add(path.join(filepath, subfile));
-                await findExtraneousFiles(path.join(filepath, subfile));
               }
             } else if (file[0] === '.' && file !== '.bin') {
               if (!(await fs.lstat(filepath)).isDirectory()) {
                 possibleExtraneous.add(filepath);
-                await findExtraneousFiles(filepath);
               }
             } else {
               possibleExtraneous.add(filepath);
-              await findExtraneousFiles(filepath);
             }
           }
         }
@@ -476,7 +473,7 @@ export default class PackageLinker {
     });
 
     // remove all extraneous files that weren't in the tree
-    for (const loc of possibleExtraneous) {      
+    for (const loc of possibleExtraneous) {
       this.reporter.verbose(this.reporter.lang('verboseFileRemoveExtraneous', loc));
       try {
         await fs.unlink(loc);
